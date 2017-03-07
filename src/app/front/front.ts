c2c32e8594804cb07cd91c9441f20ae4319c6d9c
tmsla123 <tmsla123@gmail.com>
2017-03-08 03:12:46 +0900

    chore(app): front page 구현

--- src/app/front/front.ts
+++ src/app/front/front.ts
@@ -6,25 +6,165 @@ import {Component, ElementRef} from '@angular/core';
 })
 export class Front {
   elementRef: ElementRef;
-  dotsText: DotsText;
 
   constructor(elementRef: ElementRef) {
     this.elementRef = elementRef;
   }
 
-  ngAfterContentInit() {
-    var nativeElement = this.elementRef.nativeElement;
+  ngAfterViewInit() {
+    this.initDotText();    
+  }
 
-    this.dotsText = new DotsText(
-      nativeElement,
+  initDotText() { 
+    let nativeElement = this.elementRef.nativeElement;
+    let dotsText = new DotsText(
       nativeElement.querySelectorAll('#canvas-dots')[0],
-      nativeElement.querySelectorAll('#canvas-text')[0]
+      nativeElement.querySelectorAll('#canvas-text')[0], 
+      {
+        maxDotCount: 1900,
+        getClientRect: () => {
+          return {
+            width: window.innerWidth,
+            height: window.innerHeight
+          };
+        },
+        colors: [
+          '255, 179, 186',
+          '255, 223, 186',
+          '255, 255, 186',
+          '186, 255, 201',
+          '186, 225, 255'
+        ],
+        background: '#5C3A58'
+      }
     );
+    let centerX = () => {return window.innerWidth / 2};
+    let centerY = () => {return window.innerHeight / 2};
+    let scenario = [
+      {
+        time: 5000,
+        image: {
+          src: '/assets/images/my_face.png',
+          scale: {
+            x: 2,
+            y: 2
+          },
+          x: centerX,
+          y: centerY
+        }
+      },
+      {
+        time: 10
+      },
+      {
+        time: 5000,
+        texts: [
+          {
+            text: 'I AM A',
+            font: 'bold 90px Lato',
+            x: centerX,
+            y: () => {return centerY() - 100;}
+          },
+          {
+            text: 'FRONT-END',
+            font: 'bold 90px Lato',
+            x: centerX,
+            y: centerY
+          },
+          {
+            text: 'DEVELOPER',
+            font: 'bold 90px Lato',
+            x: centerX,
+            y: () => {return centerY() + 100;}
+          }
+        ]
+      },
+      {
+        time: 10
+      },
+      {
+        time: 5000,
+        texts: [
+          {
+            text: 'HTML CSS',
+            font: 'bold 82px Lato',
+            x: centerX,
+            y: () => {return centerY() - 150;}
+          },
+          {
+            text: 'ECMASCRIPT',
+            font: 'bold 82px Lato',
+            x: centerX,
+            y: () => {return centerY() - 50;}
+          },
+          {
+            text: 'ANGULAR',
+            font: 'bold 82px Lato',
+            x: centerX,
+            y: () => {return centerY() + 50;}
+          },
+          {
+            text: 'SPECIALIST',
+            font: 'bold 82px Lato',
+            x: centerX,
+            y: () => {return centerY() + 150;}
+          }
+        ]
+      },
+      {
+        time: 10
+      }, 
+      {
+        time: 7000,
+        texts: [
+          {
+            text: 'MAKING THE WEB',
+            font: '82px Lato',
+            x: centerX,
+            y: () => {return centerY() - 250;}
+          },
+          {
+            text: 'MORE',
+            font: '82px Lato',
+            x: centerX,
+            y: () => {return centerY() - 150;}
+          },
+          {
+            text: 'NOBLESS',
+            font: '82px Lato',
+            x: centerX,
+            y: () => {return centerY() - 50;}
+          },
+          {
+            text: 'FABULOUS',
+            font: '82px Lato',
+            x: centerX,
+            y: () => {return centerY() + 50;}
+          },
+          {
+            text: 'TURBULENCE',
+            font: '82px Lato',
+            x: centerX,
+            y: () => {return centerY() + 150;}
+          },
+          {
+            text: 'GORGEOUS',
+            font: '82px Lato',
+            x: centerX,
+            y: () => {return centerY() + 250;}
+          }
+        ]
+      },
+      {
+        time: 2000
+      }
+    ];
+
+    dotsText.doSenario(scenario, true);
   }
 }
 
 class DotsText {
-  stage: any;
   dotsStage: any;
   dotsStageCtx: any;
 
@@ -41,19 +181,14 @@ class DotsText {
   textStageOffsetX: number;
   textStageOffsetY: number;
 
-  countdownFrom: number;
-  countdownTimer: number;
-  countdownRunning: boolean;
-
   dots: Dot[];
-  colors: string[];
   numberPixelCoordinates: Object[];
   circleRadius: number;
 
-  constructor(stage: any, dotsElement: any, canvasElement: any) {
-    let timerResize;
+  option: any;
 
-    this.stage = stage;
+  constructor(dotsElement: any, canvasElement: any, option: any) {
+    let timerResize;
 
     this.dotsStage = dotsElement;
     this.dotsStageCtx = dotsElement.getContext('2d');
@@ -61,14 +196,12 @@ class DotsText {
     this.textStage = canvasElement;
     this.textStageCtx = canvasElement.getContext('2d');
 
-    this.countdownFrom = 10;
-    this.countdownTimer = 2800;
-    this.countdownRunning = true;
-
     this.dots = [];
-    this.colors = ['61, 207, 236', '255, 244, 174', '255, 211, 218', '151, 211, 226'];
     this.numberPixelCoordinates = [];
     this.circleRadius = 2;
+
+    this.option = option;
+    this.dotsStage.style.backgroundColor = this.option.background;
 
     window.addEventListener('resize', () => {
       clearTimeout(timerResize);
@@ -80,59 +213,12 @@ class DotsText {
   }
 
   init() {
-    let minDotsCount;
-
     this.initSize();
-    minDotsCount = this.setTexts([
-      {
-        text: 'I am a',
-        font: 'bold 82px Lato',
-        x: this.dotsStageWidth / 2,
-        y: 130
-      },
-      {
-        text: 'FE Developer',
-        font: 'bold 118px Lato',
-        x: this.dotsStageWidth / 2,
-        y: 260
-      },
-      {
-        text: '&',
-        font: 'bold 82px Lato',
-        x: this.dotsStageWidth / 2,
-        y: 380,
-      },
-      {
-        text: 'HTML CSS',
-        font: 'bold 82px Lato',
-        x: this.dotsStageWidth / 2,
-        y: 500,
-      },
-      {
-        text: 'ECMAScript',
-        font: 'bold 82px Lato',
-        x: this.dotsStageWidth / 2,
-        y: 580,
-      },
-      {
-        text: 'Angular',
-        font: 'bold 82px Lato',
-        x: this.dotsStageWidth / 2,
-        y: 660,
-      },
-      {
-        text: 'Specialist',
-        font: 'bold 82px Lato',
-        x: this.dotsStageWidth / 2,
-        y: 740,
-      }
-    ]);
-    this.initDraw(minDotsCount + 50);
-    this.drawText();
+    this.initDraw();
   }
 
   initSize() {
-    let boundingClientRect = this.stage.getBoundingClientRect();
+    let boundingClientRect = this.option.getClientRect();
 
     this.dotsStageWidth = this.dotsStage.width = boundingClientRect.width;
     this.dotsStageHeight = this.dotsStage.height = boundingClientRect.height;
@@ -145,16 +231,16 @@ class DotsText {
     this.textStageOffsetY = (this.dotsStageHeight - this.textStageHeight) / 2;
   }
 
-  initDraw(maxDotCount) {
+  initDraw() {
     this.dots.length = 0;
 
-    for (let i = 0; i < maxDotCount; i++) {
+    for (let i = 0; i < this.option.maxDotCount; i++) {
       let dot = new Dot(
         this.dotsStageCtx,
         {
           x: this.randomNumber(0, this.dotsStageWidth),
           y: this.randomNumber(0, this.dotsStageHeight),
-          color: this.colors[this.randomNumber(1, this.colors.length)],
+          color: this.option.colors[this.randomNumber(0, this.option.colors.length)],
           alpha: .3,
           circleRadius: this.circleRadius
         }
@@ -176,55 +262,75 @@ class DotsText {
     requestAnimationFrame(this.loop.bind(this));
   }
 
-  setTexts(texts) {
-    let dotCounts = 0;
-    let imageData;
-
+  drawText(texts) {
     this.textStageCtx.clearRect(0, 0, this.textStageWidth, this.textStageHeight);
-
     this.textStageCtx.fillStyle = '#24282f';
     this.textStageCtx.textAlign = 'center';
 
     for (let i = 0; i < texts.length; i++) {
       if (texts[i].text) {
         this.textStageCtx.font = texts[i].font;
-        this.textStageCtx.fillText(texts[i].text, texts[i].x, texts[i].y);
+        this.textStageCtx.fillText(texts[i].text, texts[i].x(), texts[i].y());
       }
     }    
 
-    imageData = this.textStageCtx.getImageData(0, 0, this.textStageWidth, this.textStageHeight).data;
-    this.numberPixelCoordinates.length = 0;
-
-    for (let i = imageData.length; i >= 0; i -=4) {
-      if (imageData[i] !== 0) {
-        let x = (i / 4) % this.textStageWidth;
-        let y = Math.floor(Math.floor(i / this.textStageWidth) / 4);
-
-        if ((x && x % (this.circleRadius * 2 + 3) === 0) && 
-            (y && y % (this.circleRadius * 2 + 3) === 0)) {
-          this.numberPixelCoordinates.push({
-            x: x,
-            y: y
-          });
-
-          dotCounts++;
-        }
-      }
-    }
-
-    console.log('dotCounts ::: ', dotCounts);
-    return dotCounts; 
+    this._draw();
   }
 
-  drawText() {
-    for (let i = 0; i < this.numberPixelCoordinates.length; i++) {
-      this.animateDot(this.dots[i], this.numberPixelCoordinates[i]);
-    }
+  drawImage(image) {
+    let that = this;
+    let imageObject = new Image();
+    let hasScaleChanged;
+
+    this.textStageCtx.clearRect(0, 0, this.textStageWidth, this.textStageHeight);
+    this.textStageCtx.fillStyle = '#24282f';
+
+    imageObject.onload = () => {
+      if (image.scale) {
+        hasScaleChanged = true;
+        this.textStageCtx.save();  
+        this.textStageCtx.scale(image.scale.x, image.scale.y);
+      }
+      this.textStageCtx.drawImage(imageObject,
+        (image.x() - imageObject.width) * 0.5,
+        (image.y() - imageObject.height) * 0.5
+      );
+      
+      hasScaleChanged && this.textStageCtx.restore();
+
+      that._draw();
+    };
+
+    imageObject.src = image.src;
+  }
+
+  _draw() {
+    let imageData = this.textStageCtx.getImageData(0, 0, this.textStageWidth, this.textStageHeight).data;
+      this.numberPixelCoordinates.length = 0;
+
+      for (let i = imageData.length; i >= 0; i -=4) {
+        if (imageData[i] !== 0) {
+          let x = (i / 4) % this.textStageWidth;
+          let y = Math.floor(Math.floor(i / this.textStageWidth) / 4);
+
+          if ((x && x % (this.circleRadius * 2 + 3) === 0) && 
+              (y && y % (this.circleRadius * 2 + 3) === 0)) {
+            this.numberPixelCoordinates.push({
+              x: x,
+              y: y
+            });
+          }
+        }
+      }
+
+      for (let i = 0; i < this.numberPixelCoordinates.length; i++) {
+        this.dots[i] && this.animateDot(this.dots[i], this.numberPixelCoordinates[i]);
+      }
   }
 
   breakText() {
     for (let i = 0; i < this.numberPixelCoordinates.length; i++) {
-      this.animateDot(this.dots[i], '', 'space');
+      this.dots[i] && this.animateDot(this.dots[i], '', 'space');
     }
   }
 
@@ -252,6 +358,30 @@ class DotsText {
 
   randomNumber(min: number, max: number) {
     return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  doSenario(scenario, isRepeat) {
+    this._doPart(scenario, 0, isRepeat);
+  }
+
+  _doPart(scenario, index, isRepeat) {
+    let that = this;
+
+    if (index < scenario.length) {
+      if (scenario[index].image) {
+        that.drawImage(scenario[index].image);
+      } else if (scenario[index].texts) {
+        that.drawText(scenario[index].texts);
+      } else {
+        that.breakText();
+      }
+      
+      setTimeout(() => {
+        that._doPart(scenario, index + 1, isRepeat);
+      }, scenario[index].time);  
+    } else if (isRepeat) {
+      that.doSenario(scenario, isRepeat);
+    }
   }
 }
 
